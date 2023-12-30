5d3fdca7415bfcb497f5d2f797939643ceb699cf
Héctor Jaraba <hjaraba.secpro@gmail.com>
2023-12-30 19:35:50 +0100

    feat: place section

--- src/utils/animations.ts
+++ src/utils/animations.ts
@@ -1,2 +1,10 @@
-import gsap from "gsap";
-export const tl = gsap.timeline();
+import { gsap } from "gsap";
+
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+
+const tl = gsap.timeline();
+
+export { gsap, tl };
