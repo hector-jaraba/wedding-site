c115f9eb165e4d333163fc9103c8abde06c0602a
Héctor Jaraba <hjaraba.secpro@gmail.com>
2024-02-05 15:03:32 +0100

    feat: enable SSR revert

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
+
 import critters from "astro-critters";
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: cloudflare(),
   integrations: [tailwind(), svelte(), critters()],
   trailingSlash: "ignore",
   i18n: {
