4b05f1813849f798b556c3480a2c8fb8fa326313
Héctor Jaraba <hjaraba.secpro@gmail.com>
2024-02-05 10:55:01 +0100

    feat: critters inline-css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+
+import critters from "astro-critters";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), critters()],
   trailingSlash: "ignore",
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en", "be"],
     routing: {
-      prefixDefaultLocale: false,
-    },
-  },
+      prefixDefaultLocale: false
+    }
+  }
 });
