37a4e0525f843f80b704482b165bab8f71122e52
Héctor Jaraba <hjaraba.secpro@gmail.com>
2024-02-05 14:57:01 +0100

    feat: enable SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,23 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
 import critters from "astro-critters";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: cloudflare(),
   integrations: [tailwind(), svelte(), critters()],
   trailingSlash: "ignore",
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en", "be"],
     routing: {
-      prefixDefaultLocale: false
-    }
-  }
+      prefixDefaultLocale: false,
+    },
+  },
+  experimental: {
+    optimizeHoistedScript: true,
+  },
 });
